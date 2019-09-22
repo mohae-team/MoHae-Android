946eb3d6844f3b33a91d49297532a3632477abba
jinusong <sugwer7464@gmail.com>
2019-09-22 23:44:37 +0900

    [Add] Splash Fragment, xml
    앱 시작 전 Splash 화면을 Single Activity에 맞게 Fragment로 작성

---
+++ presentation/src/main/java/com/mohaeyo/mohae/ui/fragment/splash/SplashFragment.kt
@@ -0,0 +1,31 @@
+package com.mohaeyo.mohae.ui.fragment.splash
+
+import android.graphics.drawable.Animatable
+import android.os.Bundle
+import android.os.Handler
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.mohaeyo.mohae.R
+import kotlinx.android.synthetic.main.fragment_splash.*
+
+class SplashFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+            = inflater.inflate(R.layout.fragment_splash, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (splash.drawable as Animatable).start()
+
+        Handler().postDelayed({
+            context?.let {
+                findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+            }
+        }, 700)
+    }
+}
