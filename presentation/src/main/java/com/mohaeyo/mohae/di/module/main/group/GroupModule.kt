6d91981d22ab5e78c561bfc976df04a58e6b27de
jinusong <sugwer7464@gmail.com>
2019-09-24 11:55:56 +0900

    [Update] GroupModule과 Scope
    GroupFragment의 DI

--- presentation/src/main/java/com/mohaeyo/mohae/di/module/main/group/GroupModule.kt
+++ presentation/src/main/java/com/mohaeyo/mohae/di/module/main/group/GroupModule.kt
@@ -1,14 +1,24 @@
 package com.mohaeyo.mohae.di.module.main.group
 
-import com.mohaeyo.mohae.di.scope.MainFragmentScope
-import com.mohaeyo.mohae.viewmodel.main.group.GroupViewModelFactory
+import com.mohaeyo.mohae.di.scope.GroupFragmentScope
+import com.mohaeyo.mohae.ui.fragment.main.group.GroupDetailFragment
+import com.mohaeyo.mohae.ui.fragment.main.group.GroupDocFragment
+import com.mohaeyo.mohae.ui.fragment.main.group.GroupListFragment
 import dagger.Module
-import dagger.Provides
+import dagger.android.ContributesAndroidInjector
 
 @Module
-class GroupModule {
-    @MainFragmentScope
-    @Provides
-    fun provideViewModelFactory(): GroupViewModelFactory
-            = GroupViewModelFactory()
+abstract class GroupModule {
+
+    @GroupFragmentScope
+    @ContributesAndroidInjector(modules = [GroupListModule::class])
+    abstract fun groupListFragment(): GroupListFragment
+
+    @GroupFragmentScope
+    @ContributesAndroidInjector(modules = [GroupDocModule::class])
+    abstract fun groupDocFragment(): GroupDocFragment
+
+    @GroupFragmentScope
+    @ContributesAndroidInjector(modules = [GroupDetailModule::class])
+    abstract fun groupDetailFragment(): GroupDetailFragment
 }
