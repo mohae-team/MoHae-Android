dbdaa938465c91f4b7d046198efabe9d803898b3
jinusong <sugwer7464@gmail.com>
2019-09-25 23:44:17 +0900

    [Update] GroupDoc UI
    layout과 Fragment 업데이트

--- presentation/src/main/java/com/mohaeyo/mohae/ui/fragment/main/group/GroupDocFragment.kt
+++ presentation/src/main/java/com/mohaeyo/mohae/ui/fragment/main/group/GroupDocFragment.kt
@@ -1,11 +1,37 @@
 package com.mohaeyo.mohae.ui.fragment.main.group
 
+import android.content.Context
+import android.os.Bundle
+import android.view.View
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navGraphViewModels
 import com.mohaeyo.mohae.R
 import com.mohaeyo.mohae.base.DataBindingFragment
 import com.mohaeyo.mohae.databinding.FragmentGroupDocBinding
+import com.mohaeyo.mohae.viewmodel.main.group.GroupViewModel
 
 class GroupDocFragment: DataBindingFragment<FragmentGroupDocBinding>() {
+
+    private val viewModel: GroupViewModel by navGraphViewModels(R.id.group_nav_graph)
+
     override val layoutId: Int
         get() = R.layout.fragment_group_doc
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this, object: OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    findNavController().navigate(R.id.action_groupDocFragment_to_groupListFragment)
+                }
+            })
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.vm = viewModel
+    }
 }
