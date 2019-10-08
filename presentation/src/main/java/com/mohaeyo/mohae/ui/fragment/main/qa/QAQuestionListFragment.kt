a0826cb090927209aa15fd61ec3d3436588d1e10
jinusong <sugwer7464@gmail.com>
2019-10-08 09:58:19 +0900

    [Add] QA 기능의 Fragment
    ui간의 연결은 이전과 같이 했지만 다른 점은 이전에는 argument를 Model 전부의 데이터를 넘겨주었다면 이번엔 데이터의 전달이 많아 id 인자 하나로만 주고 받는다.

---
+++ presentation/src/main/java/com/mohaeyo/mohae/ui/fragment/main/qa/QAQuestionListFragment.kt
@@ -0,0 +1,61 @@
+package com.mohaeyo.mohae.ui.fragment.main.qa
+
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.mohaeyo.mohae.R
+import com.mohaeyo.mohae.adapter.QAQuestionListAdapter
+import com.mohaeyo.mohae.base.EndPointDataBindingFragment
+import com.mohaeyo.mohae.databinding.FragmentQaQuestionListBinding
+import com.mohaeyo.mohae.doBackAnimation
+import com.mohaeyo.mohae.doCommonAnimation
+import com.mohaeyo.mohae.model.QuestionModel
+import com.mohaeyo.mohae.viewmodel.main.qa.questionList.QAQuestionListViewModel
+import com.mohaeyo.mohae.viewmodel.main.qa.questionList.QAQuestionListViewModelFactory
+import kotlinx.android.synthetic.main.fragment_qa_question_list.*
+import javax.inject.Inject
+
+class QAQuestionListFragment: EndPointDataBindingFragment<FragmentQaQuestionListBinding>() {
+
+    @Inject
+    lateinit var factory: QAQuestionListViewModelFactory
+
+    private val viewModel by lazy { ViewModelProviders.of(this, factory).get(QAQuestionListViewModel::class.java) }
+
+    override val layoutId: Int
+        get() = R.layout.fragment_qa_question_list
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeEvent()
+        binding.vm = viewModel
+
+        binding.qaQuestionList.layoutManager = LinearLayoutManager(context)
+        binding.qaQuestionList.adapter = QAQuestionListAdapter(viewModel)
+    }
+
+    private fun observeEvent() {
+        viewModel.startListToDocEvent.observe(this, Observer {
+            qa_question_list_add_fab.doCommonAnimation(R.drawable.add_to_check)
+            qa_question_list_back_fab.doBackAnimation(true)
+            findNavController().navigate(R.id.action_QAQuestionListFragment_to_QAQuestionDocFragment)
+        })
+
+        viewModel.startListToDetailEvent.observe(this, Observer {
+            qa_question_list_add_fab.doCommonAnimation(R.drawable.add_to_answers)
+            qa_question_list_back_fab.doBackAnimation(true)
+            findNavController().navigate(R.id.action_QAQuestionListFragment_to_QAQuestionDetailFragment, getQuestionItemBundle(it))
+        })
+    }
+
+    private fun getQuestionItemBundle(questionModel: QuestionModel): Bundle {
+        val bundle = Bundle()
+        with(questionModel) { bundle.putInt("id", id) }
+
+        return bundle
+    }
+}
