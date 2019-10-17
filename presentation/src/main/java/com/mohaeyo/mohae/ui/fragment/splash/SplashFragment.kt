ab6eb64a48af5b2ec5f195fdcdc968a6e62a55ee
jinusong <sugwer7464@gmail.com>
2019-10-17 13:57:17 +0900

    [Update] SplashFragment

--- presentation/src/main/java/com/mohaeyo/mohae/ui/fragment/splash/SplashFragment.kt
+++ presentation/src/main/java/com/mohaeyo/mohae/ui/fragment/splash/SplashFragment.kt
@@ -1,36 +1,41 @@
 package com.mohaeyo.mohae.ui.fragment.splash
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.drawable.Animatable
 import android.os.Bundle
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation.findNavController
 import androidx.navigation.fragment.findNavController
 import com.mohaeyo.mohae.R
 import kotlinx.android.synthetic.main.fragment_splash.*
+import org.jetbrains.anko.support.v4.toast
+
+
 
 class SplashFragment: Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
             = inflater.inflate(R.layout.fragment_splash, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
+    override fun onStart() {
+        super.onStart()
         (splash.drawable as Animatable).start()
 
         Handler().postDelayed({
-            context?.let {
-                findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
             if (ActivityCompat.checkSelfPermission(context!!,
                     Manifest.permission.ACCESS_FINE_LOCATION) !=
                 PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 500)
             } else {
+                startApp()
             }
+        },1500)
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -44,8 +49,15 @@ class SplashFragment: Fragment() {
                 }
             }
             else -> {
+                activity!!.finish()
                 toast("위치정보사용을 허락 하지않아 앱을 중지합니다.")
             }
-        }, 1500)
+        }
+    }
+
+    private fun startApp() {
+        context?.let {
+            findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
+        }
     }
 }
