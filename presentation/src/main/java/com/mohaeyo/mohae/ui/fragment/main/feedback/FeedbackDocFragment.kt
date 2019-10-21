5012ebcb6872bc28680ec69d4148cb9d7dbeaaa3
jinusong <sugwer7464@gmail.com>
2019-10-21 19:29:09 +0900

    [Update] FeedbackDoc 머터리얼화

--- presentation/src/main/java/com/mohaeyo/mohae/ui/fragment/main/feedback/FeedbackDocFragment.kt
+++ presentation/src/main/java/com/mohaeyo/mohae/ui/fragment/main/feedback/FeedbackDocFragment.kt
@@ -2,28 +2,32 @@ package com.mohaeyo.mohae.ui.fragment.main.feedback
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import com.mohaeyo.mohae.R
-import com.mohaeyo.mohae.base.DataBindingFragment
+import com.mohaeyo.mohae.base.BaseLocationFragment
 import com.mohaeyo.mohae.databinding.FragmentFeedbackDocBinding
 import com.mohaeyo.mohae.doBackAnimation
 import com.mohaeyo.mohae.doCommonAnimation
 import com.mohaeyo.mohae.viewmodel.main.feedback.doc.FeedbackDocViewModel
 import com.mohaeyo.mohae.viewmodel.main.feedback.doc.FeedbackDocViewModelFactory
 import kotlinx.android.synthetic.main.fragment_feedback_doc.*
+import org.jetbrains.anko.support.v4.toast
 import javax.inject.Inject
 
-class FeedbackDocFragment: DataBindingFragment<FragmentFeedbackDocBinding>() {
+class FeedbackDocFragment: BaseLocationFragment<FragmentFeedbackDocBinding>() {
 
+    override val mapId: Int
+        get() = R.id.feedback_doc_search_map
 
     @Inject
     lateinit var factory: FeedbackDocViewModelFactory
 
-    private val viewModel by lazy { ViewModelProviders.of(this, factory).get(FeedbackDocViewModel::class.java) }
+    override val viewModel by lazy { ViewModelProviders.of(this, factory).get(FeedbackDocViewModel::class.java) }
 
     override val layoutId: Int
         get() = R.layout.fragment_feedback_doc
@@ -46,6 +50,21 @@ class FeedbackDocFragment: DataBindingFragment<FragmentFeedbackDocBinding>() {
 
     private fun observeEvent() {
         viewModel.startDocToListEvent.observe(this, Observer { backToList() })
+
+        viewModel.drawMarkerEvent.observe(this, Observer {
+            drawMarker(title = it.title, snippet = it.snippet, location = it.location)
+        })
+
+        viewModel.locationUpdateEvent.observe(this, Observer {
+            try { fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Handler().looper) }
+            catch (exception: SecurityException) { checkPermission() }
+        })
+
+        viewModel.summaryErrorEvent.observe(this, Observer { feedback_doc_summary_edit_lay.error = it })
+
+        viewModel.descriptionErrorEvent.observe(this, Observer { feedback_doc_description_edit_lay.error = it })
+
+        viewModel.createToastEvent.observe(this, Observer { toast(it) })
     }
 
     private fun backToList() {
--- presentation/src/main/java/com/mohaeyo/mohae/viewmodel/main/feedback/doc/FeedbackDocViewModel.kt
+++ presentation/src/main/java/com/mohaeyo/mohae/viewmodel/main/feedback/doc/FeedbackDocViewModel.kt
@@ -1,10 +1,40 @@
 package com.mohaeyo.mohae.viewmodel.main.feedback.doc
 
-import com.mohaeyo.mohae.base.BaseViewModel
+import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.maps.model.LatLng
+import com.mohaeyo.mohae.base.BaseLocationViewModel
 import com.mohaeyo.mohae.base.SingleLiveEvent
+import com.mohaeyo.mohae.model.MapMakerModel
 
-class FeedbackDocViewModel(): BaseViewModel() {
+class FeedbackDocViewModel(): BaseLocationViewModel() {
+
+    val titleText = MutableLiveData<String>()
+    val addressText = MutableLiveData<String>()
+    val summaryText = MutableLiveData<String>()
+    val descriptionText = MutableLiveData<String>()
+
     val startDocToListEvent = SingleLiveEvent<Unit>()
+    val summaryErrorEvent = SingleLiveEvent<String>()
+    val descriptionErrorEvent = SingleLiveEvent<String>()
+
+    override fun updateAddressData(
+        location: LatLng,
+        addressTitle: String,
+        addressSnippet: String,
+        isSuccess: Boolean
+    ) {
+        if (isSuccess) {
+            drawMarkerEvent.value =
+                MapMakerModel(location = location, title = addressTitle, snippet = addressSnippet)
+            titleText.value = addressTitle
+            addressText.value = addressSnippet
+        } else {
+            drawMarkerEvent.value =
+                MapMakerModel(location = location, title = "다른 지역을 선택해주세요.", snippet = "다른 지역을 선택해주세요.")
+            titleText.value = addressTitle
+            addressText.value = "다른 지역을 선택해주세요."
+        }
+    }
 
     fun clickPostFeedback() {
         startDocToListEvent.call()
